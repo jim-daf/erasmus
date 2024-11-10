8c2dea4c808fdf6a796219e2366164aac6371912
Dimitris Dafnis <68849116+jim-daf@users.noreply.github.com>
2024-11-10 19:43:19 +0200

    Update backend.js

--- backend.js
+++ backend.js
@@ -92,6 +92,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/', 'form.html'));
 });
 // Start the server
-app.listen(PORT,'192.168.1.2', () => {
+app.listen(PORT, () => {
     console.log(`Server is running on http://192.168.1.2:${PORT}`);
 });
